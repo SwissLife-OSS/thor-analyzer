832d2606e9cfa8abed6372ec3f55ce157a282a77
Michael Staib <michael.staib@hotmail.com>
2017-07-05 10:55:13 +0200

    Initial Commit

---
+++ src/EventSourceAnalyzer.Tests/EventSourceAnalyzerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.EnterpriseLibrary.SemanticLogging.Utility;
+using Xunit;
+
+namespace Microsoft.Practices.EnterpriseLibrary.SemanticLogging.Utility.Tests
+{
+    public class EventSourceAnalyzerTests
+    {
+        [Fact]
+        public void InspectEmptyEventSource()
+        {
+            // arrange
+            EmptyEventSource emptyEventSource = EmptyEventSource.Log;
+
+            // act
+            EventSourceAnalyzer.InspectAll(emptyEventSource);
+        }
+    }
+}
