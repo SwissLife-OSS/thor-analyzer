21aa62f13ccf5a4e91c6efb952f3669e32c0ee24
Rafael Staib <me@rafaelstaib.com>
2017-08-22 20:25:41 +0200

    Added config for AppVeyor build service

---
+++ build/build_script.ps1
@@ -0,0 +1,2 @@
+dotnet restore .\src\EventSourceAnalyzer.sln
+dotnet build .\src\EventSourceAnalyzer.sln -c Release
---
+++ build/test_script.ps1
@@ -0,0 +1,3 @@
+Invoke-Expression ((Get-ChildItem ($env:USERPROFILE + '\.nuget\packages\OpenCover'))[0].FullName + '\tools\OpenCover.Console.exe -register:user -target:".\build\runtests.bat" -searchdirs:".\src\*\bin\Debug\netcoreapp2.0" -oldstyle -output:coverage.xml -skipautoprops -returntargetcode -filter:"+[*Analyzer]*"')
+Invoke-Expression ((Get-ChildItem ($env:USERPROFILE + '\.nuget\packages\coveralls.io'))[0].FullName + '\tools\coveralls.net.exe --opencover coverage.xml')
+dotnet pack .\src\EventSourceAnalyzer.sln -c Release
