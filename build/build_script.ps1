8ecdaf2376bbed6ac187a8d5eb234d26474e20e7
Rafael Staib <me@rafaelstaib.com>
2017-08-23 11:27:29 +0200

    Fixed sonar issue

--- build/build_script.ps1
+++ build/build_script.ps1
@@ -9,6 +9,6 @@ foreach($info in $infos)
 }
 
 # Build & Analyse
-SonarQube.Scanner.MSBuild.exe begin /k:"EventSourceAnalyzer" /d:"sonar.host.url=https://sonarqube.com" /d:"sonar.login=" + $env:SONARQUBE_TOKEN /v:$env:APPVEYOR_REPO_TAG_NAME
+Invoke-Expression ('SonarQube.Scanner.MSBuild.exe begin /k:"EventSourceAnalyzer" /d:"sonar.host.url=https://sonarqube.com" /d:"sonar.login=' + $env:SONARQUBE_TOKEN + '" /v:"' + $env:APPVEYOR_REPO_TAG_NAME + '"')
 dotnet build .\src\EventSourceAnalyzer.sln -c $env:CONFIGURATION
-SonarQube.Scanner.MSBuild.exe end /d:"sonar.login=" + $env:SONARQUBE_TOKEN
+Invoke-Expression ('SonarQube.Scanner.MSBuild.exe end /d:"sonar.login=' + $env:SONARQUBE_TOKEN + '"')
