180c215f6cc3435946cafbb3d40817b33259881f
Rafael Staib <me@rafaelstaib.com>
2017-08-23 12:02:15 +0200

    Diabled sonar because it does not support .Net Core right now

--- build/build_script.ps1
+++ build/build_script.ps1
@@ -9,6 +9,7 @@ foreach($info in $infos)
 }
 
 # Build & Analyse
-Invoke-Expression ('SonarQube.Scanner.MSBuild.exe begin /k:"' + $env:APPVEYOR_PROJECT_NAME + '" /d:"sonar.host.url=https://sonarqube.com" /d:"sonar.login=' + $env:SONARQUBE_TOKEN + '" /v:"' + $env:APPVEYOR_REPO_TAG_NAME + '"')
+# Sonar for .Net Core is not supported right now; for more information go to https://jira.sonarsource.com/browse/SONARMSBRU-310
+#Invoke-Expression ('SonarQube.Scanner.MSBuild.exe begin /k:"' + $env:APPVEYOR_PROJECT_NAME + '" /d:"sonar.host.url=https://sonarqube.com" /d:"sonar.login=' + $env:SONARQUBE_TOKEN + '" /v:"' + $env:APPVEYOR_REPO_TAG_NAME + '"')
 dotnet build .\src\EventSourceAnalyzer.sln -c $env:CONFIGURATION
-Invoke-Expression ('SonarQube.Scanner.MSBuild.exe end /d:"sonar.login=' + $env:SONARQUBE_TOKEN + '"')
+#Invoke-Expression ('SonarQube.Scanner.MSBuild.exe end /d:"sonar.login=' + $env:SONARQUBE_TOKEN + '"')
--- build/install.ps1
+++ build/install.ps1
@@ -1,1 +1,2 @@
-choco install msbuild-sonarqube-runner -y
+# Sonar for .Net Core is not supported right now; for more information go to https://jira.sonarsource.com/browse/SONARMSBRU-310
+#choco install msbuild-sonarqube-runner -y
